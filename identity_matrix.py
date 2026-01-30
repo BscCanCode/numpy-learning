7092537e26c9762b01959d784dd62fd7364a42f8
Siddhesh Tambe <xabc31101@gmail.com>
2026-01-30 21:22:17 +0530

    Add example creating an identity matrix using NumPy eye

---
+++ identity_matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def show_identity_matrix(identity_matrix):
+    print("Identity_Matrix: ", identity_matrix)
+
+identity_matrix = np.eye(3)
+
+show_identity_matrix(identity_matrix)
