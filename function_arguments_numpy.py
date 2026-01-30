2415d5983ba36636d029bec79105579f3953c2e6
Siddhesh Tambe <xabc31101@gmail.com>
2026-01-30 20:13:45 +0530

    Add function to display Python list and NumPy array using parameters

---
+++ function_arguments_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+def show(li, array_op):
+    print(f"List: {li}")
+    print(f"array_numpy: {array_op}")
+
+li = [1,2,3,4,55]
+array_op = np.array([1,2,3,4,55])
+
+show(li, array_op)
