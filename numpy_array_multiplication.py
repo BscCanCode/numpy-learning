e216c1b3a42ffe847d4ece02fb747375d15f3959
Siddhesh Tambe <xabc31101@gmail.com>
2026-02-01 01:03:37 +0530

    Demonstrate NumPy scalar and element-wise array multiplication

---
+++ numpy_array_multiplication.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr = np.array([[7,9,5],
+                [8,5,4]])
+
+result = arr * 2
+print(result)
+
+print()
+arr2 = np.array([[8,5,4],
+                 [9,4,9],
+                 ])
+arr3 = np.array([[8,0,6], [9,7,4]])
+
+result1 = arr2 * arr3
+print(result1)
